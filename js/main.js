7881030d292ee564406646e80b2236a9ed7158b9
nikthecreator <nikifor.shapran@gmail.com>
2018-09-23 16:55:06 +0300

    сщььше

---
+++ js/main.js
@@ -0,0 +1,8 @@
+$(function(){
+    $('a[data-target^="anchor"]').bind('click.smoothscroll', function(){
+        var target = $(this).attr('href'),
+            bl_top = $(target).offset().top;
+        $('body, html').animate({scrollTop: bl_top}, 700);
+        return false;
+    })
+})
