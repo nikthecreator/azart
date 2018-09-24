4d6e7e2122a9216c4ecf875d751b6ca5e2a7296d
nikthecreator <nikifor.shapran@gmail.com>
2018-09-24 22:53:03 +0300

    commit

--- js/main.js
+++
@@ -1,8 +0,0 @@
-$(function(){
-    $('a[data-target^="anchor"]').bind('click.smoothscroll', function(){
-        var target = $(this).attr('href'),
-            bl_top = $(target).offset().top;
-        $('body, html').animate({scrollTop: bl_top}, 700);
-        return false;
-    })
-})
